70357f82de5f102fccf91a60dfaa5ea3db15b899
Denikaev Emil <edenikaev@mail.ru>
2022-03-25 22:06:50 +0400

    Музыка, ответы при нажатии

--- js/main.js
+++ js/main.js
@@ -1,1 +1,55 @@
 "use strict"
+
+let correct = [];
+
+let users = [];
+
+function blue() {
+  playSound('do');
+  usersAnswer(1);
+}
+function red() { 
+  playSound('re');
+  usersAnswer(2);
+}
+
+function green() {
+  playSound('mi');
+  usersAnswer(3);
+}
+
+function yellow() {
+  playSound('fa');
+  usersAnswer(4);
+}
+
+function level(time) {
+
+}
+
+function startGame() {
+  randomNumber()
+}
+
+function playSound(tile) {
+  var audio = new Audio(`sounds/${tile}.mp3`);
+  audio.play();	
+}
+
+function randomNumber() {
+  return correctAnswer(Math.floor((Math.random()*4)+1));
+}
+
+function correctAnswer(answer) {
+  
+  correct.push(answer);
+  console.log(correct);
+}
+
+function usersAnswer(answer) {
+  
+  users.push(answer);
+  console.log(users);
+}
+
+
